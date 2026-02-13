2bcfb1442d10885d772f3bb29d300ddd2db9d2b7
muhallilahnaf <muhallilahnaf@gmail.com>
2026-02-13 14:58:55 +0600

    added and fixed charts, winner data

--- src/routes/election-2026/+page.svelte
+++ src/routes/election-2026/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Chart from 'chart.js/auto';
+	import ChartDataLabels from 'chartjs-plugin-datalabels';
 	import PageHeading from '$lib/components/PageHeading.svelte';
     import menuData from '$lib/data/menuData.json'
 	import { onMount } from 'svelte';
@@ -17,64 +18,66 @@
 	let chartPartyVotePc = null
 	let partyVotePc = $state([])
 
+	let canvasPartyAvgVotePc = $state(null)
+	let chartPartyAvgVotePc = null
+	let partyAvgVotePc = $state([])
+
+	let canvasSeatAvgVotePc = $state(null)
+	let chartSeatAvgVotePc = null
+	let seatAvgVotePc = $state([])
+
 	let seatVotesData = $state([])
-	let seatVotesPAData = $state([])
-	let seatVotesPADataLatest = $state([])
 	let partyVotesData = $state([])
-	const bigp = ['BNP', 'Jamaat', 'Islami Andolan Bangladesh', 'NCP', 'JaPa']
+	// const bigp = ['BNP', 'Jamaat', 'Islami Andolan Bangladesh', 'NCP', 'JaPa']
 	const colors = {
 		'BNP': '#F2C744', 
-		'Jamaat': '#004c55', 
+		'Jamaat': '#009036', 
 		'Islami Andolan Bangladesh': '#671933', 
 		'NCP': '#004c55', 
-		'JaPa': '#ed1b24'
+		'Independent': '#ed1b24',
 	}
 
 
 	onMount(async () => {
-		// seatVotesData = await readCSV('https://raw.githubusercontent.com/muhallilahnaf/election-2026-live/master/data/seat_votes.csv')
-		seatVotesPAData = await readCSV('https://raw.githubusercontent.com/muhallilahnaf/election-2026-live/master/data/seat_votes_pa.csv')
-		partyVotesData = await readCSV('https://raw.githubusercontent.com/muhallilahnaf/election-2026-live/master/data/party_votes.csv')
+		Chart.register(ChartDataLabels);
+		
+		// fetch data
+		seatVotesData = await readCSV('https://raw.githubusercontent.com/muhallilahnaf/election-2026-live/master/data/seat_votes_pa.csv')
+		partyVotesData = await readCSV('https://raw.githubusercontent.com/muhallilahnaf/election-2026-live/master/data/party_votes_pa.csv')
 		// console.log(seatVotesData.length);
-		if (seatVotesPAData.length > 0) {
-			let latest = new Date(seatVotesPAData[0].time)
-			seatVotesPAData.forEach(d => {
-				if (new Date(d.time) > latest) {
-					latest = new Date(d.time)
-				}
-			})
-			seatVotesPADataLatest = seatVotesPAData.filter(d => new Date(d.time) == latest)
-			console.log(seatVotesPAData);
-			
-		}
-		partyWins = partyVotesData.filter(
-			q => bigp.includes(q.party)
-		).toSorted(
-			(a, b) => b.count - a.count
-		)		
+		
+		// party win count
+		partyWins = partyVotesData.filter(p => p.count > 0).toSorted((a, b) => b.count - a.count)		
 		if (canvasPartyWins) {
 			chartPartyWins = new Chart(canvasPartyWins, {
 				type: 'bar',
 				data: {
-					labels: [''],
-					datasets: partyWins.map(p => ({
-						label: p.party,
-						data: [p.count],
-						backgroundColor: colors[p.party]
-					}))
+					labels: partyWins.map(p => p.party),
+					datasets: [{
+						label: '',
+						data: partyWins.map(p => p.count),
+						backgroundColor: partyWins.map(p => {
+							if (p.party in colors) return colors[p.party]
+							return '#d3d3d3'
+						}),
+						datalabels: {
+							color: '#333333',
+							font: {
+								weight: 'bold'
+							}
+      					}
+					}]
 				},
 				options: {
 					indexAxis: 'y',
 					responsive: true,
 					scales: {
 						x: {
-							stacked: true,
 							grid: {
-                				display: false
+                				display: true
             				}
 						},
 						y: {
-							stacked: true,
 							grid: {
                 				display: false
             				}
@@ -83,11 +86,9 @@
 				}
 			})
 		}
-		partyVotePc = partyVotesData.filter(
-			q => bigp.includes(q.party)
-		).toSorted(
-			(a, b) => b.vote_pc - a.vote_pc
-		)
+
+		// party total vote pc
+		partyVotePc = partyVotesData.toSorted((a, b) => b.total_vote_pc - a.total_vote_pc)
 		if (canvasPartyVotePc) {
 			chartPartyVotePc = new Chart(canvasPartyVotePc, {
 				type: 'pie',
@@ -95,8 +96,18 @@
 					labels: partyVotePc.map(p => p.party),
 					datasets: [{
 						label: 'Vote %',
-						data: partyVotePc.map(p => p.vote_pc),
-						backgroundColor: partyVotePc.map(p => colors[p.party])
+						data: partyVotePc.map(p => p.total_vote_pc),
+						backgroundColor: partyVotePc.map(p => {
+							if (p.party in colors) return colors[p.party]
+							return '#d3d3d3'
+						}),
+						datalabels: {
+        					anchor: 'end',
+							color: '#333333',
+							font: {
+								weight: 'bold'
+							}
+      					}
 					}]
 				},
 				options: {
@@ -107,8 +118,90 @@
 							position: 'bottom',
 							align: 'center'
 						},
-					}
+						datalabels: {
+							formatter: (value, context) => {
+								if (value > 0.3) return value
+								return ''
+							}
+						}
+    				},
+				}
+			})
+		}
+
+		// party avg vote pc
+		partyAvgVotePc = partyVotesData.toSorted((a, b) => b.avg_vote_pc - a.avg_vote_pc)
+		if (canvasPartyAvgVotePc) {
+			chartPartyAvgVotePc = new Chart(canvasPartyAvgVotePc, {
+				type: 'pie',
+				data: {
+					labels: partyAvgVotePc.map(p => p.party),
+					datasets: [{
+						label: 'Avg Vote %',
+						data: partyAvgVotePc.map(p => p.avg_vote_pc),
+						backgroundColor: partyAvgVotePc.map(p => {
+							if (p.party in colors) return colors[p.party]
+							return '#d3d3d3'
+						}),
+						datalabels: {
+        					anchor: 'end',
+							color: '#333333',
+							font: {
+								weight: 'bold'
+							}
+      					}
+					}]
 				},
+				options: {
+					responsive: true,
+					plugins: {
+						legend: {
+							display: true,
+							position: 'bottom',
+							align: 'center'
+						},
+						datalabels: {
+							formatter: (value, context) => {
+								if (value > 10) return value
+								return ''
+							}
+						}
+    				},
+				}
+			})
+		}
+
+		// seat average vote pc
+		let seatAvgVotePcData = {}
+		seatVotesData.forEach(s => {
+			let vote_pc = parseFloat(s.vote_pc)
+			vote_pc = Number.isNaN(vote_pc) ? 0 : vote_pc
+			if (s.seat in seatAvgVotePcData) {
+				seatAvgVotePcData[s.seat].push(vote_pc)
+			} else {
+				seatAvgVotePcData[s.seat] = [vote_pc]
+			}
+		})		
+		seatAvgVotePc = Object.values(seatAvgVotePcData).map(s => s.reduce((acc, current) => acc+current, 0) / s.length)
+		seatAvgVotePc = seatAvgVotePc.toSorted((a, b) => b-a)
+		if (canvasSeatAvgVotePc) {
+			chartSeatAvgVotePc = new Chart(canvasSeatAvgVotePc, {
+				type: 'line',
+				data: {
+					labels: seatAvgVotePc.map(s => ''),
+					datasets: [{
+						label: 'Avg Vote %',
+						data: Object.values(seatAvgVotePc),
+					}],
+				},
+				options: {
+					responsive: true,
+					plugins: {
+						datalabels: {
+        					display: false,
+						}
+					}
+				}
 			})
 		}
 	})
@@ -167,29 +260,11 @@
 	// seat top 2 candidates votes
     let seatTopCandidates = $derived(
 		seatVotesData.filter(
-			s => s.seat_name === selectedSeat
+			s => s.seat === selectedSeat
 		).toSorted(
 			(a, b) => b.vote - a.vote
 		).slice(0, 2)
     )
-
-	const getPALatest = (data) => {
-		if (data.length > 0) {
-			// console.log(data);
-			let latest = new Date(data[0].time)
-			// console.log(latest);
-			
-			data.forEach(d => {
-				if (new Date(d.time) > latest) {
-					latest = new Date(d.time)
-				}
-			})
-			return data.filter(d => new Date(d.time) == latest)
-		}
-		return []
-	}
-
-	// let seatVotesPADataLatest = $derived(getPALatest(seatVotesPAData))
 
     $effect(() => {
 		divisions = getDivisions(seatsDataStore.seatsData)
@@ -220,32 +295,24 @@
 			<h3>Party ahead/won</h3>
 			<canvas bind:this={canvasPartyWins}></canvas>
 		</div>
-		<!-- pie chart party vote % -->
+		<!-- pie chart party total vote % -->
 		<div class="ui basic very padded segment canvas">
 			<h3>Party vote % (out of total voters)</h3>
 			<canvas bind:this={canvasPartyVotePc}></canvas>
 		</div>
-		<!-- all seats result -->
-		<table>
-			<thead>
-				<tr>
-					<th>seat</th>
-					<th>party</th>
-					<th>vote</th>
-				</tr>
-			</thead>
-			<tbody>
-				{#each seatVotesPADataLatest as data}
-					<tr>
-						<td>{data.seat}</td>
-						<td>{data.party}</td>
-						<td>{data.vote}</td>
-					</tr>
-				{/each}
-			</tbody>
-		</table>
+		<!-- pie chart party avg vote % -->
+		<div class="ui basic very padded segment canvas">
+			<h3>Party avg vote % (in their contested seats)</h3>
+			<canvas bind:this={canvasPartyAvgVotePc}></canvas>
+		</div>
+		<!-- line chart seat avg vote % -->
+		<div class="ui basic very padded segment canvas">
+			<h3>Seat avg vote % (avg among top 2 candidates)</h3>
+			<canvas bind:this={canvasSeatAvgVotePc}></canvas>
+		</div>
 		<!-- dropdowns -->
 		<div class="ui basic very padded segment">
+			<h3>Select a seat</h3>
 			<select class="ui selection dropdown" bind:value={selectedDivision}>
 				<option value={undefined} disabled selected>Select a division</option>
 				{#each divisions as division}
